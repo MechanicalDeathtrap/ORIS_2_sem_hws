f0a4d4a533ff5280c1c7531350936ab6a534d539
MechanicalDeathtrap <korikova-04@mail.ru>
2024-05-11 23:48:38 +0300

    adding hw

---
+++ TeamHost_seventh_hw/TeamHost/Areas/GameProfile/Controllers/GameProfileController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeamHost.WebAPI.Areas.GameProfile.Controllers
+{
+    [Route("[controller]")]
+    public class GameProfileController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public GameProfileController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("{gameName}")]
+        public async Task<IActionResult> Index(string gameName)
+        {
+            var getGameQuery = new GetGameQuery(gameName);
+            var game = await _mediator.Send(getGameQuery);
+
+            return View(game);
+        }
+    }
+}
---
+++ TeamHost_seventh_hw/TeamHost/Areas/GameProfile/Views/Home/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TeamHost.WebAPI.Areas.GameProfile;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
